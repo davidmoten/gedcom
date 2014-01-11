a0d9dee8bb5f33b3702330f62d7c8a5fe5c9a1f7
Dave Moten <davidmoten@gmail.com>
2014-01-11 23:42:03 +1100

    change to scala regex

--- src/main/scala/com/github/davidmoten/gedcom/grammar/grammar.scala
+++ src/main/scala/com/github/davidmoten/gedcom/grammar/grammar.scala
@@ -2,48 +2,89 @@ package com.github.davidmoten.gedcom.grammar
 
 trait Element
 sealed trait Depth
-sealed trait Multiplicity
+case class AnyDepth extends Depth
+case class DepthZero extends Depth
+case class RelativeDepth(depth: Int) extends Depth
+sealed trait MultiplicityBound
+case class Specific(value: Integer) extends MultiplicityBound {
+  require(value >= 0)
+}
+case class Unbounded extends MultiplicityBound
+case class Multiplicity(min: MultiplicityBound, max: MultiplicityBound)
 case class Tag(value: String)
-case class Value(depth: Depth, tag: Tag, name: String, mult: Multiplicity) extends Element
+case class Value(depth: Depth, tag: Tag,
+  name: String, mult: Multiplicity) extends Element
+case class Id(depth: Depth, tag: Tag, id: String) extends Element
 case class Definition(name: String, node: Node)
-case class Grammar(root: Definition, definitions: List[Definition])
+case class DefinitionLine(name: String) extends Element
 case class Node(level: Int, element: Element, children: Node)
+case class DefinitionReference(depth: Depth, name: String) extends Element
+case class Grammar(root: Definition, definitions: List[Definition])
+
+object RegexHelper {
+  import java.util.regex._
+
+  def matcher(pattern: Pattern, line: String): (Option[String], Option[Matcher]) = {
+    val m = pattern.matcher(line)
+    if (!m.find)
+      (Some(line), None)
+    else
+      (None, Some(m))
+  }
+}
 
 private object Line {
+
   import java.util.regex._
+  import RegexHelper._
 
   private val multPatternEnding = "\\s*{(\\d:(\\d|M))}\\s*$"
 
-  private val valuePattern = Pattern.compile(
-    "^\\s*(0|n|(\\+\\d+))\\s+(\\w+)\\s+<(\\w+)>" +
-      multPatternEnding, Pattern.DOTALL)
+  private val ValuePattern =
+    ("^\\s*(0|n|(\\+\\d+))\\s+(\\w+)\\s+<(\\w+)>" +
+      multPatternEnding).r
 
-  private val idPattern = Pattern.compile("^\\s*(0|n|(\\+\\d+))\\s+(\\w+)" +
-    multPatternEnding, Pattern.DOTALL)
-  private val definitionPattern = Pattern.compile(
-    "^\\s*(\\w+): =\\s*$", Pattern.DOTALL)
-  private val definitionReferencePatter = Pattern.compile(
+  private val IdPattern = ("^\\s*(0|n|(\\+\\d+))\\s+(\\w+)" +
+    multPatternEnding).r
+
+  private val DefinitionPattern = "^\\s*(\\w+): =\\s*$".r
+
+  private val DefinitionReferencePattern = (
     "^\\s*(0|n|(\\+\\d+))\\s+<<(\\w+)>>" +
-      multPatternEnding, Pattern.DOTALL)
+    multPatternEnding).r
+
+  private def parseDefinitionLine(line: String) = {
+    DefinitionPattern.findFirstMatchIn(line).map(m => DefinitionLine(m.group(1)))
+  }
+
+  private def parseValue(line: String) = {
+    ValuePattern.findFirstMatchIn(line).map(
+      m => Value(AnyDepth(), Tag("SOUR"), "fred", Multiplicity(Specific(1), Unbounded())))
+  }
 
   //1=relative level,4=def ref, 6=tag, 9=xref,10=name, 11=min,12=max
   def parse(line: String) = {
-    val m = valuePattern.matcher(line)
-    if (!m.find)
-      None
-    else
-      Some(line)
+    (parseDefinitionLine(line) ++
+      parseValue(line))
+      .headOption
+  }
+
+  def parse(is: java.io.InputStream) = {
+    io.Source.fromInputStream(is)
+      .getLines
+
+    null
   }
 }
 
 class Parser(is: java.io.InputStream) {
 
   def parse = {
-    val empty:(List[List[String]],List[String])= (List(), List())
+    val empty: (List[List[String]], List[String]) = (List(), List())
     io.Source.fromInputStream(is)
       .getLines
       .filter(_.trim.length > 0)
-      .foldLeft(empty)((g,line) => g)
+      .foldLeft(empty)((g, line) => g)
   }
 
 }
